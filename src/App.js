a46e12550dbe69bd758ae3ca5f7165014c457b13
talha <talha@gmail.com>
2022-10-08 14:17:16 +0300

    first-react-work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import Msg from "./components/Msg";
+import Person from "./components/Person";
+import data from "./data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data.map((person) =>
+      (<Person
+         key={person.name}
+       name = {person.name} 
+       img = {person.img} 
+       tel = {person.tel} 
+       />))}
     </div>
   );
 }
---
+++ src/components/Msg.jsx
@@ -0,0 +1,14 @@
+const Msg = ({name}) => {
+  //   console.log(props);
+
+  //! Destructuring
+//   const { name } = props;
+  return (
+    <div>
+      <h3>Merhaba {name}</h3>
+      {/* <h3>Merhaba {props.name}</h3> */}
+    </div>
+  );
+};
+
+export default Msg;
